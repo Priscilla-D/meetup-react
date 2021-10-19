4677873ec4d5e40d3b4ef3e066f52f6ce83edbc7
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-19 14:04:11 +0200

    section 2-31

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import Todo from './components/Todo';
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-<div>
-  <h1>my todos</h1>
-    <Todo text='Learn React' />
-    <Todo text='Master React' />
-    <Todo text='Explore the full React course' />
-</div>
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 

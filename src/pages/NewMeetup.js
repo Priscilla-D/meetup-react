c181de592f1d4f125d7b7fbe35481c7e895b46d3
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-19 17:08:05 +0200

    section 2-40

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,24 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-
-    function addMeetupHandler(meetupData) {}
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-51e24-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json"
+        },
+      }
+    );
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
+  );
 }
 
 export default NewMeetupPage;

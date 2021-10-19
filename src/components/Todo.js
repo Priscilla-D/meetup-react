f086114ce4af282376d2640a5ed1f220a7bdea7f
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-19 12:02:07 +0200

    section 2-29

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+    const [ modalIsOpen, setModalIsOpen] = useState(false);
     function deleteHandler() {
-        console.log('cliked!');
-        console.log(props.text);
+        setModalIsOpen(true);
     }
 return(
     <div className='card'>
@@ -9,8 +13,11 @@ return(
     <div className='actions'>
       <button className='btn' onClick={deleteHandler}>Delete</button>
     </div>
+    { modalIsOpen && <Modal /> }
+    { modalIsOpen && <Backdrop />}
+    
   </div>
-)
+);
 }
 
 export default Todo;

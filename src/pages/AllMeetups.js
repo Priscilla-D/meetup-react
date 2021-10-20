82c5cf051284debcab89139698261213df0bc310
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-20 11:34:57 +0200

    section 2-43

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,47 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-getting-started-51e24-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
-  )
-    .then((response) => {
-      response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-51e24-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+          meetups.push(meetup);
+          
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+        console.log(meetups);
+      });
+  }, []);
 
-if(isLoading) {
-    return <section>
+  if (isLoading) {
+    return (
+      <section>
         <p>Loading...</p>
-    </section>
-}
+      </section>
+    );
+  }
 
   return (
     <section>
